e5ad9febb9b2fa825d6d713c515fb214998fe92e
Derek Miller <dreemkiller@gmail.com>
2020-12-20 20:49:46 -0600

    GUI is now responding to events in the floor plan area (using a GtkEventBox around it)

--- light_controller.go
+++ light_controller.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"os"
 
+	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 )
 
 var signals = map[string]interface{}{
-	"on_floor_button_clicked": on_floor_button_clicked,
+	"on_floor_button_clicked":         on_floor_button_clicked,
+	"floorplan_button_press_event_cb": floorplan_button_press_event_cb,
+	"floorplan_touch_event_cb":        floorplan_touch_event_cb,
 }
 
 func main() {
@@ -19,6 +22,12 @@ func main() {
 	if err != nil {
 		println("Failed to create application")
 		log.Fatalln("Couldn't create app:", err)
+	}
+
+	first_floor_pix, err := gdk.PixbufNewFromFileAtScale("Floorplan_first_1bpp.bmp", 427, 320, true)
+	if err != nil {
+		println("Failed to create pix from file:", err)
+		log.Fatalln("Failed to create pix from file:", err)
 	}
 
 	println("Attempting to connect activate")
@@ -32,17 +41,24 @@ func main() {
 
 		builder.ConnectSignals(signals)
 
+		floorplan_area, err := builder.GetObject("Floorplan")
+		if err != nil {
+			println("Failed to get object Floorplan:", err)
+			log.Fatalln("Failed to get objec Floorplan:", err)
+
+		}
+
+		if drawable_floorplan_area, ok := floorplan_area.(*gtk.Image); ok {
+			drawable_floorplan_area.SetFromPixbuf(first_floor_pix)
+		} else {
+			println("Floorplan area is NOT image")
+		}
+
 		obj, err := builder.GetObject("Top")
 		if err != nil {
 			println("Failed to get object Top")
 			log.Fatalln("Coultn'd get object Top")
 		}
-
-		// obj, err = builder.GetObject("window")
-		// if err != nil {
-		// 	println("Failed to get object window")
-		// 	log.Fatalln("Couldn't getObject window")
-		// }
 
 		println("Alls good so far")
 		wnd := obj.(*gtk.Window)
@@ -55,4 +71,12 @@ func main() {
 
 func on_floor_button_clicked() {
 	println("Floor button clicked")
+}
+
+func floorplan_button_press_event_cb() {
+	println("Floorplan pressed")
+}
+
+func floorplan_touch_event_cb() {
+	println("Floorplan touch event")
 }
